b10928a3451e0f959d7a38a045e1a863036e446a
lic10 <lic10@rpi.edu>
2016-06-16 17:26:30 -0400

    add triple generator java script

---
+++ TripleGenerator/TripleGenerator.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+
+public class TripleGenerator {	
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		String[] times = {
+				"13:32:53.4", 
+				"13:33:03.4", 
+				"13:33:13.2", 
+				"13:33:24.0", 
+				"13:33:34.5", 
+				"13:33:45.3", 
+				"13:33:55.8", 
+				"13:34:06.6", 
+				"13:34:17.2", 
+				"13:34:27.9", 
+				"13:34:38.5", 
+				"13:34:49.2", 
+				"13:34:59.8", 
+				"13:35:10.5", 
+				"13:35:21.2", 
+				"13:35:31.9", 
+				"13:35:42.5", 
+				"13:35:53.2", 
+				"13:36:03.8", 
+				"13:36:14.5", 
+				"13:36:25.1", 
+				"13:36:35.8", 
+				"13:36:46.4", 
+				"13:36:57.1", 
+				"13:37:07.8", 
+				"13:37:18.5", 
+				"13:37:29.1", 
+				"13:37:39.8", 
+				"13:37:50.4", 
+				"13:38:01.1", 
+				"13:38:11.7", 
+				"13:38:22.4", 
+				"13:38:33.5", 
+				"13:38:44.4"};	
+		String date = "2016-05-10T";
+		
+		Triple triple = new Triple(times, date);
+		File file = new File("process_triples.ttl");
+		FileUtils.writeStringToFile(file, triple.getTriples());
+	}
+}
